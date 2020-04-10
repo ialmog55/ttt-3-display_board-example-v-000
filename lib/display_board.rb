a86aeec29ea32a17185fa40e7dee70d707467a39
idith almog <almog.idith@gmail.com>
2020-04-10 20:24:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,10 @@
-def diaplay_board
-  tttboard= [" "," "," "," "," "," "," "," "," "]
-  print tttboard
+def display_board
 
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
 end# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+display_board
